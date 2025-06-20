e0fa5cafdae3b6c456dfaccade49cf9e434d9dbe
Louis <Louis@vietmeier.de>
2025-06-21 00:32:05 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
-
 import {Routes} from '@angular/router';
 import {NotFoundComponent} from './pages/functions/not-found/not-found';
 import {DefaultComponent} from './pages/dashboard/default/default';
-import { ManagementComponent } from './pages/dashboard/management/management';
+import {ManagementComponent} from './pages/dashboard/management/management';
 import {LandingComponent} from './pages/landing/landing';
+import {TestChartComponent} from './charts/test-chart/test-chart';
 
 export const routes: Routes = [
-  { path: '', component: LandingComponent },
-  { path: 'dashboard/default', component: DefaultComponent },
-  { path: 'dashboard/management', component: ManagementComponent },
-  { path: '**', component: NotFoundComponent }
+  {path: '', component: LandingComponent},
+  {path: 'dashboard/default', component: DefaultComponent},
+  {path: 'dashboard/management', component: ManagementComponent},
+  {path: 'chart', component: TestChartComponent},
+  {path: '**', component: NotFoundComponent}
 ];
---
+++ src/app/charts/test-chart/test-chart.ts
@@ -0,0 +1,145 @@
+import {Component, ViewChild} from '@angular/core';
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid,
+  ApexAnnotations, NgApexchartsModule
+} from "ng-apexcharts";
+import {series} from '../../data';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  annotations: ApexAnnotations;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  labels: string[];
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+};
+
+@Component({
+  selector: 'app-test-chart',
+  imports: [
+    ChartComponent
+  ],
+  templateUrl: './test-chart.html',
+  styleUrl: './test-chart.css'
+})
+export class TestChartComponent {
+
+  @ViewChild("chart") chart!: ChartComponent;
+
+  public chartOptions: ChartOptions;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "series",
+          data: series.monthDataSeries4.prices
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      annotations: {
+        yaxis: [
+          {
+            y: 50,
+            borderColor: "#00E396",
+            label: {
+              borderColor: "#00E396",
+              style: {
+                color: "#fff",
+                background: "#00E396"
+              },
+              text: "Green Zone"
+            }
+          },
+          {
+            y: 0,
+            y2: 30,
+            borderColor: "#000",
+            fillColor: "#FEB019",
+            opacity: 0.2,
+            label: {
+              borderColor: "#333",
+              style: {
+                fontSize: "10px",
+                color: "#333",
+                background: "#FEB019"
+              },
+              text: "Danger Zone"
+            }
+          }
+        ],
+        xaxis: [
+          {
+            x: new Date("01 Nov 2022").getTime(),
+            strokeDashArray: 0,
+            borderColor: "#775DD0",
+            label: {
+              borderColor: "#775DD0",
+              style: {
+                color: "#fff",
+                background: "#775DD0"
+              },
+              text: "Anno Test"
+            }
+          },
+          {
+            x: new Date("01 Nov 2023").getTime(),
+            x2: new Date("01 Nov 2023").getTime(),
+            fillColor: "#B3F7CA",
+            opacity: 0.4,
+            label: {
+              borderColor: "#B3F7CA",
+              style: {
+                fontSize: "10px",
+                color: "#fff",
+                background: "#00E396"
+              },
+              offsetY: -10,
+              text: "X-axis range"
+            }
+          }
+        ],
+        points: [
+
+        ]
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+      grid: {
+        padding: {
+          right: 30,
+          left: 20
+        }
+      },
+      title: {
+        text: "Line with Annotations",
+        align: "left"
+      },
+      labels: series.monthDataSeries4.dates,
+      xaxis: {
+        type: "datetime"
+      }
+    };
+  }
+}
